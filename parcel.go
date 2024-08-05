38c615afb4a5ac99e39a258aecc5677d6af50730
Farkat Sayfullin <fsayfullin@rteam.tech>
2024-08-05 17:56:55 +0300

    actions

--- parcel.go
+++ parcel.go
@@ -35,6 +35,7 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 
 	row := s.db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = :number",
 		sql.Named("number", number))
+
 	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
 	if err != nil {
 		return p, err
